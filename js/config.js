0cd79684384cd4ae02c5247e7d408028d09c39c1
jokame <jokame@gmail.com>
2014-10-15 05:36:46 -0500

    Primera liberacion

---
+++ js/config.js
@@ -0,0 +1,11 @@
+var config = {
+    'client_id': '485111268922-h2tvs4ue56khfa4hfpb8mm93iqfcv9bk.apps.googleusercontent.com',
+    'scope': ['https://www.googleapis.com/auth/drive', 
+	'https://www.googleapis.com/auth/drive.appdata', 
+	'https://www.googleapis.com/auth/drive.apps.readonly', 
+	'https://www.googleapis.com/auth/drive.file', 
+	'https://www.googleapis.com/auth/drive.metadata.readonly', 
+	'https://www.googleapis.com/auth/drive.readonly']
+	    
+    }
+var apiKey = 'AIzaSyC_IRpidAa-TiyNM2gqJd27tXhNdso0OoI';
